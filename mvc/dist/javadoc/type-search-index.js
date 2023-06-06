08c2b729321c5a35b73db1aca3d89e2d683596ae
Abdur Rahman <abdurrahmanshaikh830@gmail.com>
2023-06-06 19:52:39 +0530

    mvc project added

---
+++ mvc/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"BusinessModel"},{"p":"model","l":"FirstModel"},{"p":"controller","l":"FrontController"}];updateSearchResults();
